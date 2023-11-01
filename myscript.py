a1328d4d95b83b8f67d567887c45643b48475268
Stephan Zwicknagl <stephanzwicknagl@gmx.de>
2023-11-01 09:40:48 -1000

    initial commit

---
+++ myscript.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Hi there, this is {name}")
+
+def add(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    greet("myscript")
+    print(add(2, 3))
+    print(f"Let's get a documentation for this")
